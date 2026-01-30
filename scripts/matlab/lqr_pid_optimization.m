d7a4ba59f65028365e221780d24cb29aca8e2082
Jan Łuczka <jasiu.luczka@gmail.com>
2026-01-30 21:38:19 +0100

    final

--- scripts/matlab/lqr_pid_optimization.m
+++ scripts/matlab/lqr_pid_optimization.m
@@ -1,26 +1,21 @@
-% LQR & DLQR PID Optimization for DC Motor
-% Porównanie: Model II rzędu (PID fizyczny) vs Model I rzędu (PID matematyczny z pamięcią)
 clear; clc; close all;
 
-%% 1. Parametry układu
-Ts = 0.020;         % Sampling time [s]
+Ts = 0.020;
 k_fi = 0.004916;    
 b = 0.00000869;     
 R = 7.5;            
 L = 0.0001;         
 m_car = 0.5;        
 m_wheel = 0.05;     
-r_wheel = 0.032;    % (Poprawiłem r_wheel na 0.032 zgodnie z pierwszą wersją, w drugiej było 0.05)
-
+r_wheel = 0.032;
 J = (0.5 * m_wheel * r_wheel^2) + (m_car * r_wheel^2);
 
-% --- MODEL 1: Pełny (II rzędu - uwzględnia L) ---
+% --- MODEL 1: Pełny (II rzędu) ---
 Am = [-b/J, k_fi/J; -k_fi/L, -R/L];
 Bm = [0; 1/L];
 Q = diag([100, 10, 0.1]); 
 R_weight = 1;
 
-%% 2A. Projekt sterownika DLQR -> PID (Model II rzędu)
 sys_m = ss(Am, Bm, [1, 0], 0);
 sys_md = c2d(sys_m, Ts, 'zoh');
 Ad = sys_md.A;
@@ -41,30 +36,21 @@ fprintf('========================================\n');
 fprintf('   MODEL PEŁNY (II RZĄD\n');
 fprintf('Kp = %.4f, Ki = %.4f, Kd = %.4f\n', Kp, Ki, Kd);
 
-%% 2B. Projekt sterownika DLQR -> PID (Model I rzędu z RÓŻNICZKOWANIEM)
 K_stat = 13.77;    
 tau = 2.3;  
 
-% Model ciągły
 Am_1 = -1/tau;
 Bm_1 = K_stat/tau;
 
-% Dyskretyzacja
+
 sys_m1 = ss(Am_1, Bm_1, 1, 0);
 sys_md1 = c2d(sys_m1, Ts, 'zoh');
 Ad1 = sys_md1.A;
 Bd1 = sys_md1.B;
 
-% Rozszerzenie o stan poprzedniej prędkość" ---
-% Wektor stanu: x = [int_e; omega(k); omega(k-1)]
-% Równania:
-% 1) int_e(k+1) = int_e(k) - Ts*omega(k) + Ts*ref
-% 2) omega(k+1) = Ad1*omega(k) + Bd1*u(k)
-% 3) omega(k)   = omega(k)  <-- to wchodzi do "omega(k-1)" w następnym kroku
-
-A_aug1 = [1,   -Ts,    0;      % Integrator
-          0,   Ad1,    0;      % Fizyka modelu (I rząd)
-          0,    1,     0];     % Pamięć: x3(k+1) = x2(k)
+A_aug1 = [1,   -Ts,    0;
+          0,   Ad1,    0;
+          0,    1,     0];
 
 B_aug1 = [0; Bd1; 0];
 B_ref1 = [Ts; 0; 0];
@@ -75,14 +61,6 @@ R_weight1 = 1;
 
 Kd_lqr1 = dlqr(A_aug1, B_aug1, Q1, R_weight1);
 
-% --- MAPOWANIE NA PID (DYSKRETNE) ---
-% LQR zwraca u = -k1*CALKA - k2*OMEGA - k3*OMEGA_OLD
-% PID dyskretny: u = Ki*SumaE - Kp*OMEGA - Kd*(OMEGA - OMEGA_OLD)/Ts
-% Rozpisując PID: u = Ki*SumaE - (Kp + Kd/Ts)*OMEGA + (Kd/Ts)*OMEGA_OLD
-% Porównując z LQR:
-% 1) k3 = -Kd/Ts  -> Kd = -k3 * Ts
-% 2) k2 = Kp + Kd/Ts -> Kp = k2 - Kd/Ts = k2 + k3
-
 Ki_1 = -Kd_lqr1(1);
 Kd_1 = -Kd_lqr1(3) * Ts;
 Kp_1 = Kd_lqr1(2) + Kd_lqr1(3); 
@@ -91,21 +69,19 @@ fprintf('========================================\n');
 fprintf('   MODEL UPROSZCZONY (I RZĄD)   \n');
 fprintf('Kp = %.4f, Ki = %.4f, Kd = %.4f\n', Kp_1, Ki_1, Kd_1);
 
-%% 3. Symulacja zamkniętego układu
+
 setpoint = 10; 
 t_final = 5;
 
-% --- Symulacja II rząd ---
 C_cl = [0, 1, 0; -Kd_lqr]; 
 sys_cl = ss(A_aug - B_aug*Kd_lqr, B_ref, C_cl, [0;0], Ts);
 [y_sim, t_out] = step(setpoint * sys_cl, t_final); 
 
-% --- Symulacja I rząd (z nowymi macierzami 3x3) ---
-C_cl1 = [0, 1, 0; -Kd_lqr1]; % Wyjścia: [omega, u]
+
+C_cl1 = [0, 1, 0; -Kd_lqr1];
 sys_cl1 = ss(A_aug1 - B_aug1*Kd_lqr1, B_ref1, C_cl1, [0;0], Ts);
 [y_sim1, t_out1] = step(setpoint * sys_cl1, t_final);
 
-%% 4. Wykresy
 figure('Color', 'w', 'Name', 'Porównanie PID');
 
 subplot(2,1,1);
